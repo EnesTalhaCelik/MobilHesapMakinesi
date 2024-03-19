ea0bf1d2f1e6b5ee410a4a2d6148c26709408ef1
Enes Talha ÇELİK <enes.talha@hotmail.com.tr>
2024-03-19 17:35:14 +0300

    2. commit

--- app/src/main/java/com/example/hesapmakinesiodev/MainActivity.java
+++ app/src/main/java/com/example/hesapmakinesiodev/MainActivity.java
@@ -12,11 +12,14 @@ import java.util.ArrayList;
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
 
-
-
-
-
-
+    private int sayiUzunluk,toplamUzunluk = 1; // büyüdükçe texti küçült ve bir değişken kullan
+    private String semboller[] = {"+","-","/","%"};
+    private String sembollerHesap[] = {"+","-","/","%"};
+    private String hesapYazisi;
+    private int artEksTik = 0;
+    int sonYapilanIslem = 1; // değer 0 ise cursorun sol tarafında bir işlem yoktur eğer 1 ise cursorun
+    //sol tarafında matematiksel operatör vardır ve bir operatör eklendiğinde cursorun solundaki operatör
+    //değiştirilecektir.
 
 
 
@@ -35,6 +38,17 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         Button butn7 =  findViewById(R.id.btn7);
         Button butn8 =  findViewById(R.id.btn8);
         Button butn9 =  findViewById(R.id.btn9);
+        Button butnTemizle = findViewById(R.id.btnC);
+        Button butnNokta = findViewById(R.id.btnNokt);
+        Button btnEsittir = findViewById(R.id.btnEsittir);
+        Button butnTopla =  findViewById(R.id.btnTopla);
+        Button butnCikar =  findViewById(R.id.btnCikar);
+        Button butnCarp =  findViewById(R.id.btnCarp);
+        Button butnBol =  findViewById(R.id.btnBol);
+        Button butnYuzde =  findViewById(R.id.btnYuzde);
+        Button ButnSil = findViewById(R.id.ButtonSil);
+        Button ButnGecmis = findViewById(R.id.ButtonGecmis);
+        Button ButnArtEks = findViewById(R.id.btnArtEks);
         TextView txt_Sonuc = findViewById(R.id.textViewSonuc);
         TextView txt_Islem = findViewById(R.id.textViewIslem);
 
@@ -55,16 +69,238 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
 
 
+
         for(Button b:nums){
             b.setOnClickListener(view ->{
                 if(!txt_Islem.getText().toString().equals("0")){
                     txt_Islem.setText(txt_Islem.getText().toString()+b.getText().toString());
-                }else{
+                    /*
+                    sayiUzunluk +=1;
+                    toplamUzunluk += 1;*/
+                    //bunun yeirne text lenght
+                    sonYapilanIslem = 0;
+                } else
+                {
                     txt_Islem.setText(b.getText().toString());
+                    sonYapilanIslem = 0;
+
                 }
 
             });
         }
+        btnEsittir.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //eşittir fonksiyonu
+
+            }
+        });
+
+        ButnArtEks.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if(sonYapilanIslem == 0){
+                    if(artEksTik == 0){
+                        artEksTik = 1;
+                        txt_Islem.setText(txt_Islem.getText().toString()+"+");
+                    }else {
+                        artEksTik = 0;
+                        txt_Islem.setText(txt_Islem.getText().toString()+"-");
+                    }
+
+
+
+
+
+                }else{
+
+                    if(txt_Islem.getText().toString().charAt(txt_Islem.getText().toString().length() - 1) == '+'){
+                        txt_Islem.setText(txt_Islem.getText().toString().substring(0, txt_Islem.length() - 1) + '-') ;
+                        artEksTik = 0;
+                    } else {
+                        txt_Islem.setText(txt_Islem.getText().toString().substring(0, txt_Islem.length() - 1) + '+') ;
+                        artEksTik = 1;
+                    }
+                }
+
+                sonYapilanIslem = 1;
+
+            }
+        });
+
+
+
+        butnCarp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if(!txt_Islem.getText().toString().equals("0")&&sonYapilanIslem == 0){
+                    txt_Islem.setText(txt_Islem.getText().toString()+"x");
+
+                    /*sayiUzunluk =0;
+                    toplamUzunluk += 1;*/
+                }else if(!txt_Islem.getText().toString().equals("0")&&sonYapilanIslem == 1){
+                    //txt_Islem.setText(txt_Islem.getText().toString()+b.getText().toString());
+                    //burada cursor pozisyonunu kullanarak işlem değişikliği yapacağız
+                    /*sayiUzunluk =0;
+                    toplamUzunluk += 1;*/
+                }
+
+                sonYapilanIslem = 1;
+
+            }
+        });
+
+        butnTopla.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if(!txt_Islem.getText().toString().equals("0")&&sonYapilanIslem == 0){
+                    txt_Islem.setText(txt_Islem.getText().toString()+"+");
+
+                    /*sayiUzunluk =0;
+                    toplamUzunluk += 1;*/
+                }else if(!txt_Islem.getText().toString().equals("0")&&sonYapilanIslem == 1){
+                    //txt_Islem.setText(txt_Islem.getText().toString()+b.getText().toString());
+                    //burada cursor pozisyonunu kullanarak işlem değişikliği yapacağız
+                    /*sayiUzunluk =0;
+                    toplamUzunluk += 1;*/
+                }
+
+                sonYapilanIslem = 1;
+
+            }
+        });
+
+        butnNokta.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if(!txt_Islem.getText().toString().equals("0")&&sonYapilanIslem == 0){
+                    txt_Islem.setText(txt_Islem.getText().toString()+".");
+
+                    /*sayiUzunluk =0;
+                    toplamUzunluk += 1;*/
+                }else if(!txt_Islem.getText().toString().equals("0")&&sonYapilanIslem == 1){
+                    //txt_Islem.setText(txt_Islem.getText().toString()+b.getText().toString());
+                    //burada cursor pozisyonunu kullanarak işlem değişikliği yapacağız
+                    /*sayiUzunluk =0;
+                    toplamUzunluk += 1;*/
+                }
+
+                sonYapilanIslem = 1;
+
+            }
+        });
+
+
+
+
+        butnCikar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if(!txt_Islem.getText().toString().equals("0")&&sonYapilanIslem == 0){
+                    txt_Islem.setText(txt_Islem.getText().toString()+"-");
+
+                    /*sayiUzunluk =0;
+                    toplamUzunluk += 1;*/
+                }else if(!txt_Islem.getText().toString().equals("0")&&sonYapilanIslem == 1){
+                    //txt_Islem.setText(txt_Islem.getText().toString()+b.getText().toString());
+                    //burada cursor pozisyonunu kullanarak işlem değişikliği yapacağız
+                    /*sayiUzunluk =0;
+                    toplamUzunluk += 1;*/
+                }
+
+                sonYapilanIslem = 1;
+
+            }
+        });
+        butnBol.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if(!txt_Islem.getText().toString().equals("0")&&sonYapilanIslem == 0){
+                    txt_Islem.setText(txt_Islem.getText().toString()+"/");
+
+                    /*sayiUzunluk =0;
+                    toplamUzunluk += 1;*/
+                }else if(!txt_Islem.getText().toString().equals("0")&&sonYapilanIslem == 1){
+                    //txt_Islem.setText(txt_Islem.getText().toString()+b.getText().toString());
+                    //burada cursor pozisyonunu kullanarak işlem değişikliği yapacağız
+                    /*sayiUzunluk =0;
+                    toplamUzunluk += 1;*/
+                }
+
+                sonYapilanIslem = 1;
+
+            }
+        });
+
+        ButnSil.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if(!txt_Islem.getText().toString().equals("0")&&txt_Islem.getText().toString().length() != 1){
+                    txt_Islem.setText(txt_Islem.getText().toString().substring(0, txt_Islem.length() - 1));
+
+                    /*sayiUzunluk =0;
+                    toplamUzunluk += 1;*/
+                }else if(txt_Islem.getText().toString().length() == 1){
+                    txt_Islem.setText("0");
+                }else if(txt_Islem.getText().toString().length() == 0){
+                    txt_Islem.setText("0");
+                }
+
+                if( txt_Islem.getText().toString().charAt(txt_Islem.getText().toString().length() - 1) == '+'||
+                        txt_Islem.getText().toString().charAt(txt_Islem.getText().toString().length() - 1)== '-'||
+                        txt_Islem.getText().toString().charAt(txt_Islem.getText().toString().length() - 1)=='x'||
+                        txt_Islem.getText().toString().charAt(txt_Islem.getText().toString().length() - 1)== '/'||
+                        txt_Islem.getText().toString().charAt(txt_Islem.getText().toString().length() - 1)== '%'||
+                        txt_Islem.getText().toString().charAt(txt_Islem.getText().toString().length() - 1)== '.'){
+                    sonYapilanIslem = 1;
+                }else{
+                    sonYapilanIslem = 0;
+                }
+
+            }
+
+        });
+
+        butnYuzde.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if(!txt_Islem.getText().toString().equals("0")&&sonYapilanIslem == 0){
+                    txt_Islem.setText(txt_Islem.getText().toString()+"%");
+
+                    /*sayiUzunluk =0;
+                    toplamUzunluk += 1;*/
+                }else if(!txt_Islem.getText().toString().equals("0")&&sonYapilanIslem == 1){
+                    //txt_Islem.setText(txt_Islem.getText().toString()+b.getText().toString());
+                    //burada cursor pozisyonunu kullanarak işlem değişikliği yapacağız
+                    /*sayiUzunluk =0;
+                    toplamUzunluk += 1;*/
+                }
+
+                sonYapilanIslem = 1;
+
+            }
+        });
+        butnTemizle.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+                    txt_Islem.setText("0");
+                    sonYapilanIslem = 0;
+                    sayiUzunluk = 1;
+
+
+            }
+        });
+
 
     }
 
